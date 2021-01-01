c4ebd44a62620640f4b0f96b45dfe995c7e2cf95
Tactryl3 <71281742+Tactryl3@users.noreply.github.com>
2021-01-01 01:25:16 -0500

    search

--- index.js
+++ index.js
@@ -78,27 +78,4 @@ function timeConverter(UNIX_timestamp){
 	console.warn('Something went wrong.', err);
 });
 
-//Table Searching
-function tableSearch() {
-  // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("tbody");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 1; i < tr.length; i++) { //Changed i = 1 to show top row
-    td = tr[i].getElementsByTagName("td")[1]; //Brackets at end indicate what column
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-
 
